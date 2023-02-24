202bebc32ddf5e2d85db3674d6cd450c47dd062d
ideologis <akinwalesosina99@gmail.com>
2023-02-23 18:31:44 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: .input integer parameter
- *
- * code by prince solomon
+ * print_number - Prints an integer
+ * @n: Parameter
  */
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int i, j, count;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		i = -1;
+		i = n * -1;
 	}
-	if (i / 10)
+	else
 	{
-		print_number(i / 10);
+		i = n;
 	}
-	_putchar(i % 10 + '0');
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar (((i / count) % 10) + 48);
+	}
 }
